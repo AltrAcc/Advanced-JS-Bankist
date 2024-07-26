0c4534f711e22a85435aacda0c9dff1644c0259f
Janvi-wb <janvirupapara426@gmail.com>
2024-07-26 18:25:19 +0530

    added feature - sticky nav

--- script.js
+++ script.js
@@ -129,6 +129,29 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+///////////////////////////////////////
+// 015 Implementing a Sticky Navigation_ The Scroll Event
+// Sticky navigation: Intersection Observer API
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
 ////////////////////////////////////////////////////
 // 005 Selecting, Creating, and Deleting Elements
 
